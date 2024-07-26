d6b8916316af3852607e515c73f46f463dffe0e9
Christian Zaldaña <chris.zaldana92@gmail.com>
2024-07-25 18:14:11 -0600

    Ejercicio 1 completado

---
+++ Ejercicio1/ex1.js
@@ -0,0 +1,20 @@
+const btn1 = document.getElementById('btn1')
+
+btn1.addEventListener('click', () =>{
+    const bgcolor = document.getElementById('bgcolor')
+    bgcolor.style.backgroundColor = "red"
+})
+
+const btn2 = document.getElementById('btn2')
+
+btn2.addEventListener('click', () =>{
+    const bgcolor1 = document.getElementById('bgcolor1')
+    bgcolor1.style.backgroundColor = "green"
+})
+
+const btn3 = document.getElementById('btn3')
+
+btn3.addEventListener('click', () =>{
+    const bgcolor2 = document.getElementById('bgcolor2')
+    bgcolor2.style.backgroundColor = "blue"
+})
